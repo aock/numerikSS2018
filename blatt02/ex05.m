bc927915ca0f7863b832cc3214788b73ee9fc780
Alexander Mock <amock@uos.de>
2018-04-18 11:18:46 +0200

    added ex05

---
+++ blatt02/ex05.m
@@ -0,0 +1,16 @@
+%%% Aufgabe 5
+
+%% a)
+
+r1 = somefunc(2,5);
+r2 = somefunc(pi,7);
+r3 = somefunc(sqrt(2),4);
+
+fprintf('somefunc(2,5) = %e\n', r1);
+fprintf('somefunc(pi,7) = %e\n', r2);
+fprintf('somefunc(sqrt(2),4) = %e\n', r3);
+
+
+%% b)
+
+dbtype('somefunc.m');
