1027fb0b41f23dd1c91db240f1dc7b6d1db323c2
Alexander Mock <amock@uos.de>
2018-05-23 20:05:00 +0200

    ordnung

--- blatt07/ex02.m
+++ blatt07/ex02.m
@@ -52,23 +52,48 @@ end
 
 iter_sek = 1:1:length(e_x_sek);
 
+
+% Ordnungen des Newton- und Sekantenverfahrens bestimmen
+ord_newton = @(k) log(e_x_newton(k)) / log(e_x_newton(k-1));
+ord_sek = @(k) log(e_x_sek(k)) / log(e_x_sek(k-1));
+
+ord_newton_vec = [];
+for i = 2:length(e_x_newton)
+    ord_newton_vec = [ord_newton_vec, ord_newton(i)];
+end
+
+ord_sek_vec = [];
+for i = 2:length(e_x_sek)
+    ord_sek_vec = [ord_sek_vec, ord_sek(i)];
+end
+
+
 % plotting
 
 figure
-ax1 = subplot(2,1,1);
+ax1 = subplot(2,2,1);
 semilogy(ax1,iter_newton,e_x_newton)
 title(ax1,'Newton Verfahren')
 ylabel(ax1,'Fehler')
 xlabel(ax1,'Iteration')
 
-ax2 = subplot(2,1,2);
+ax2 = subplot(2,2,2);
 semilogy(ax2,iter_sek,e_x_sek)
 title(ax2,'Sekanten Verfahren')
 ylabel(ax2,'Fehler')
 xlabel(ax2,'Iteration')
 
+ax3 = subplot(2,2,3);
+plot(ax3,iter_newton(2:end),ord_newton_vec)
+title(ax3,'Ordnung Newtonverfahren')
+ylabel(ax3,'Ordnung')
+xlabel(ax3,'Iteration')
 
-
+ax4 = subplot(2,2,4);
+plot(ax4,iter_sek(2:end),ord_sek_vec)
+title(ax4,'Ordnung Sekantenverfahren')
+ylabel(ax4, 'Ordnung')
+xlabel(ax4, 'Iteration')
 
 
 
