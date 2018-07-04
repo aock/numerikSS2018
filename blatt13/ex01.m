2cd55dfbe4ae9996ca29f10f3a29ffc4bc2c88fb
Alexander Mock <amock@uos.de>
2018-07-04 17:43:01 +0200

    added comments

--- blatt13/ex01.m
+++ blatt13/ex01.m
@@ -1,3 +1,5 @@
+%% Aufgabe 1 b)
+
 x_k_func = @(k) k;
 
 x = linspace(-1,4,101);
@@ -9,8 +11,8 @@ N_2_0 = splineN(2,0,x_k_func,x);
 figure(1)
 plot(x,N_0_0)
 hold on
-plot(x,N_1_0)
-plot(x,N_2_0)
+plot(x,N_1_0,'--')
+plot(x,N_2_0,'-o')
 hold off
 legend('N^0_0','N^1_0','N^2_0');
 xlabel('x');
